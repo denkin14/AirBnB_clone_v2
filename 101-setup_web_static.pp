f46d51bf734fd098522f4d7e8bfc353b86308e32
denkin14 <dennismuguongo@gmail.com>
2022-11-02 10:34:59 +0000

    puppet fo setup

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,43 @@
+# This  Puppet script will install nginx and change configuration files
+$directories = '/data/web_static/releases/test /data/web_static/shared'
+$location_block = '\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html;\n\t}'
+
+# install nginx x
+exec { 'nginx':
+  command  => 'sudo apt -y update && sudo apt install -y nginx',
+  provider => 'shell'
+}
+
+# create required directory and files
+exec {'directories':
+  command  => "sudo mkdir -p ${directories}",
+  provider => 'shell'
+}
+
+-> exec {'index':
+  command  => 'echo "It is the way" | sudo tee /data/web_static/releases/test/index.html',
+  provider => 'shell'
+}
+
+-> exec {'link':
+  command  => 'sudo ln -sf /data/web_static/releases/test /data/web_static/current',
+  provider => 'shell'
+}
+
+# change permissions
+-> exec {'user':
+  command  => 'sudo chown -R ubuntu:ubuntu /data',
+  provider => 'shell'
+}
+
+# configure nginx
+-> exec {'config':
+  command  =>  "sudo sed -i '38i\\${location_block}' /etc/nginx/sites-available/default",
+  provider => 'shell'
+}
+
+# restart nginx
+-> exec {'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell'
+}
